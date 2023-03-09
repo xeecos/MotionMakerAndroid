8192e30364451c99520cecfe2eb75921503bc8ee
虎子哥 <flashindream@gmail.com>
2023-03-09 17:23:35 +0800

    set ip address

--- app/src/main/java/com/xeecos/motionmaker/PreviewActivity.java
+++ app/src/main/java/com/xeecos/motionmaker/PreviewActivity.java
@@ -2,6 +2,9 @@ package com.xeecos.motionmaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,14 +15,32 @@ public class PreviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
+        Button settingBt = (Button)findViewById(R.id.buttonSetting);
+        settingBt.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(PreviewActivity.this, SettingActivity.class);   //Intent intent=new Intent(MainActivity.this,JumpToActivity.class);
+                 startActivity(intent);
+             }
+        });
         Button previewBt = (Button)findViewById(R.id.buttonPreview);
         previewBt.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
+
+                SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+                String ip = sp.getString("ipaddress", "192.168.43.204");
                 EzImageView imageView = (EzImageView) findViewById(R.id.imageView);
                 imageView.setImageDrawable(null);
-                imageView.setImageURL("http://192.168.43.204/preview/latest.jpg");
+                imageView.setImageURL("http://"+ip+"/preview/latest.jpg");
+            }
+        });
+        Button timelapseBt = (Button)findViewById(R.id.buttonTimelapse);
+        timelapseBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PreviewActivity.this, TimelapseActivity.class);   //Intent intent=new Intent(MainActivity.this,JumpToActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/xeecos/motionmaker/SettingActivity.java
+++ app/src/main/java/com/xeecos/motionmaker/SettingActivity.java
@@ -1,8 +1,18 @@
 package com.xeecos.motionmaker;
 
+import static android.widget.Toast.*;
+
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +20,65 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        EditText txt_ip1 = (EditText)findViewById(R.id.editTextIP1);
+        EditText txt_ip2 = (EditText)findViewById(R.id.editTextIP2);
+        EditText txt_ip3 = (EditText)findViewById(R.id.editTextIP3);
+        EditText txt_ip4 = (EditText)findViewById(R.id.editTextIP4);
+        txt_ip1.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String ipstr = txt_ip1.getText()+"."+txt_ip2.getText()+"."+txt_ip3.getText()+"."+txt_ip4.getText();
+                editor.putString("ipaddress", ipstr);
+                editor.apply();
+                return false;
+            }
+        });
+        txt_ip2.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String ipstr = txt_ip1.getText()+"."+txt_ip2.getText()+"."+txt_ip3.getText()+"."+txt_ip4.getText();
+                editor.putString("ipaddress", ipstr);
+                editor.apply();
+                return false;
+            }
+        });
+        txt_ip3.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String ipstr = txt_ip1.getText()+"."+txt_ip2.getText()+"."+txt_ip3.getText()+"."+txt_ip4.getText();
+                editor.putString("ipaddress", ipstr);
+                editor.apply();
+                return false;
+            }
+        });
+        txt_ip4.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String ipstr = txt_ip1.getText()+"."+txt_ip2.getText()+"."+txt_ip3.getText()+"."+txt_ip4.getText();
+                editor.putString("ipaddress", ipstr);
+                editor.apply();
+                return false;
+            }
+        });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+        String ip = sp.getString("ipaddress", "192.168.43.204");
+
+        EditText txt_ip1 = (EditText)findViewById(R.id.editTextIP1);
+        EditText txt_ip2 = (EditText)findViewById(R.id.editTextIP2);
+        EditText txt_ip3 = (EditText)findViewById(R.id.editTextIP3);
+        EditText txt_ip4 = (EditText)findViewById(R.id.editTextIP4);
+        String[] separated = ip.split("\\.");
+        txt_ip1.setText(separated[0]);
+        txt_ip2.setText(separated[1]);
+        txt_ip3.setText(separated[2]);
+        txt_ip4.setText(separated[3]);
+//        Toast.makeText(this,separated[0].toLowerCase(), Toast.LENGTH_SHORT).show();
     }
 }
