0b4f31ec0c43b30229bc67117c65e921807fda9f
虎子哥 <flashindream@gmail.com>
2023-03-10 11:25:56 +0800

    update

--- app/src/main/java/com/xeecos/motionmaker/PreviewActivity.java
+++ app/src/main/java/com/xeecos/motionmaker/PreviewActivity.java
@@ -1,13 +1,27 @@
 package com.xeecos.motionmaker;
 
+import static java.lang.Float.parseFloat;
+import static java.lang.Math.floor;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class PreviewActivity extends AppCompatActivity {
 
@@ -35,6 +49,15 @@ public class PreviewActivity extends AppCompatActivity {
                 imageView.setImageURL("http://"+ip+"/preview/latest.jpg");
             }
         });
+        Button shotBt = (Button)findViewById(R.id.buttonShot);
+        shotBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+                String ip = sp.getString("ipaddress", "192.168.43.204");
+                request("http://"+ip+"/capture/shot");
+            }
+        });
         Button timelapseBt = (Button)findViewById(R.id.buttonTimelapse);
         timelapseBt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +66,110 @@ public class PreviewActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        EditText txt_gainr = (EditText)findViewById(R.id.editTextGainR);
+        EditText txt_gaing = (EditText)findViewById(R.id.editTextGainG);
+        EditText txt_gainb = (EditText)findViewById(R.id.editTextGainB);
+        EditText txt_exposure = (EditText)findViewById(R.id.editTextExp);
+
+        txt_gainr.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                refreshResult();
+                return false;
+            }
+        });
+        txt_gaing.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                refreshResult();
+                return false;
+            }
+        });
+        txt_gainb.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                refreshResult();
+                return false;
+            }
+        });
+        txt_exposure.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                refreshResult();
+                return false;
+            }
+        });
+    }
+
+    private void refreshResult()
+    {
+        SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        EditText txt_gainr = (EditText)findViewById(R.id.editTextGainR);
+        EditText txt_gaing = (EditText)findViewById(R.id.editTextGainG);
+        EditText txt_gainb = (EditText)findViewById(R.id.editTextGainB);
+        EditText txt_exposure = (EditText)findViewById(R.id.editTextExp);
+
+
+        float gainr = parseFloat(txt_gainr.getText().toString());
+        float gaing = parseFloat(txt_gaing.getText().toString());
+        float gainb = parseFloat(txt_gainb.getText().toString());
+        float exposure = parseFloat(txt_exposure.getText().toString());
+
+        editor.putString("gainr", ""+gainr);
+        editor.putString("gaing", ""+gaing);
+        editor.putString("gainb", ""+gainb);
+        editor.putString("exposure", ""+exposure);
+        editor.apply();
+
+        String ip = sp.getString("ipaddress", "192.168.43.204");
+        request("http://"+ip+"/storage/set?mode=camera");
+        request("http://"+ip+"/capture/set?coarse="+(exposure*1000)+"&fine=1&r_gain="+gainr+"&gr_gain="+gaing+"&gb_gain="+gaing+"&b_gain="+gainb);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+        float gainr = parseFloat(sp.getString("gainr", "4"));
+        float gaing = parseFloat(sp.getString("gaing", "4"));
+        float gainb = parseFloat(sp.getString("gainb", "4"));
+        float exposure = parseFloat(sp.getString("exposure", "1"));
+
+        EditText txt_exposure = (EditText) findViewById(R.id.editTextExp);
+        EditText txt_gainr = (EditText) findViewById(R.id.editTextGainR);
+        EditText txt_gaing = (EditText) findViewById(R.id.editTextGainG);
+        EditText txt_gainb = (EditText) findViewById(R.id.editTextGainB);
+
+        txt_exposure.setText(""+exposure);
+        txt_gainr.setText(""+gainr);
+        txt_gaing.setText(""+gaing);
+        txt_gainb.setText(""+gainb);
+
+    }
+    private void request(String url)
+    {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Toast.makeText(PreviewActivity.this, response.substring(0,50), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(PreviewActivity.this,"error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
--- app/src/main/java/com/xeecos/motionmaker/TimelapseActivity.java
+++ app/src/main/java/com/xeecos/motionmaker/TimelapseActivity.java
@@ -1,8 +1,27 @@
 package com.xeecos.motionmaker;
 
+import static java.lang.Float.parseFloat;
+import static java.lang.Math.floor;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class TimelapseActivity extends AppCompatActivity {
 
@@ -10,5 +29,136 @@ public class TimelapseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timelapse);
+
+
+        EditText txt_fps = (EditText)findViewById(R.id.editTextFPS);
+        EditText txt_during = (EditText)findViewById(R.id.editTextDuring);
+        EditText txt_videotime = (EditText)findViewById(R.id.editTextVideoTime);
+        EditText txt_exposure = (EditText)findViewById(R.id.editTextExposure);
+
+        EditText txt_totaltime = (EditText)findViewById(R.id.editTextTotalTime);
+        EditText txt_frames = (EditText)findViewById(R.id.editTextFrames);
+
+        txt_fps.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                refreshResult();
+                return false;
+            }
+        });
+        txt_during.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                refreshResult();
+                return false;
+            }
+        });
+        txt_videotime.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                refreshResult();
+                return false;
+            }
+        });
+        txt_exposure.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                refreshResult();
+                return false;
+            }
+        });
+
+        Button startBt = (Button)findViewById(R.id.buttonStart);
+        startBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+                String ip = sp.getString("ipaddress", "192.168.43.204");
+                request("http://"+ip+"/task/start");
+            }
+        });
+
+    }
+    private void refreshResult()
+    {
+        SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        EditText txt_fps = (EditText)findViewById(R.id.editTextFPS);
+        EditText txt_during = (EditText)findViewById(R.id.editTextDuring);
+        EditText txt_videotime = (EditText)findViewById(R.id.editTextVideoTime);
+        EditText txt_exposure = (EditText)findViewById(R.id.editTextExposure);
+
+        EditText txt_totaltime = (EditText)findViewById(R.id.editTextTotalTime);
+        EditText txt_frames = (EditText)findViewById(R.id.editTextFrames);
+
+        float fps = parseFloat(txt_fps.getText().toString());
+        float during = parseFloat(txt_during.getText().toString());
+        float videotime = parseFloat(txt_videotime.getText().toString());
+        float exposure = parseFloat(txt_exposure.getText().toString());
+        txt_frames.setText(""+(int)(videotime*fps));
+        float totaltime = ((during+exposure+2)*videotime*fps);
+        int hours = (int)(floor(totaltime/3600.0));
+        int minutes = (int)(totaltime/60-hours*60);
+        txt_totaltime.setText(""+(hours)+"小时"+(minutes)+"分");
+        editor.putString("fps", ""+fps);
+        editor.putString("during", ""+during);
+        editor.putString("videotime", ""+videotime);
+        editor.putString("exposure", ""+exposure);
+        editor.apply();
+        String ip = sp.getString("ipaddress", "192.168.43.204");
+        request("http://"+ip+"/task/add?delay=1000&during="+(during*1000)+"&count="+(int)(videotime*fps)+"&mode=3&exposure="+(int)(exposure*1000));
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sp = getSharedPreferences("ezsetting", Context.MODE_PRIVATE);
+        float fps = parseFloat(sp.getString("fps", "25"));
+        float during = parseFloat(sp.getString("during", "1"));
+        float videotime = parseFloat(sp.getString("videotime", "30"));
+        float exposure = parseFloat(sp.getString("exposure", "1"));
+
+        EditText txt_fps = (EditText)findViewById(R.id.editTextFPS);
+        EditText txt_during = (EditText)findViewById(R.id.editTextDuring);
+        EditText txt_videotime = (EditText)findViewById(R.id.editTextVideoTime);
+        EditText txt_exposure = (EditText)findViewById(R.id.editTextExposure);
+
+        EditText txt_totaltime = (EditText)findViewById(R.id.editTextTotalTime);
+        EditText txt_frames = (EditText)findViewById(R.id.editTextFrames);
+
+        txt_fps.setText(""+fps);
+        txt_during.setText(""+during);
+        txt_videotime.setText(""+videotime);
+        txt_exposure.setText(""+exposure);
+        txt_frames.setText(""+(int)(videotime*fps));
+        float totaltime = ((during+exposure+2)*videotime*fps);
+        int hours = (int)(floor(totaltime/3600.0));
+        int minutes = (int)(totaltime/60-hours*60);
+        txt_totaltime.setText(""+(hours)+"小时"+(minutes)+"分");
+//        Toast.makeText(this,separated[0].toLowerCase(), Toast.LENGTH_SHORT).show();
+//        request("http://www.baidu.com");
+    }
+    private void request(String url)
+    {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Toast.makeText(TimelapseActivity.this, response.substring(0,50), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(TimelapseActivity.this,"error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
